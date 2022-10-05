f429f6c19bd588f62607825a99fa541f18f4b0ca
shu-rt <shu.kirby.rt@gmail.com>
2022-10-05 10:42:20 +0900

    Organizes file includes and epf's

--- software/STM32F103RBT6/shu-poyo/Core/Src/main.c
+++ software/STM32F103RBT6/shu-poyo/Core/Src/main.c
@@ -22,8 +22,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "stdio.h"
-#include "parameters.h"
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
