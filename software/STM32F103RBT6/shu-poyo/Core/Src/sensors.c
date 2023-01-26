d5149f83e2459d15fae0da0cf40689aaf02c10d3
shu-rt <shu.kirby.rt@gmail.com>
2023-01-26 10:14:11 +0900

    Adds the sensor reading process

--- software/STM32F103RBT6/shu-poyo/Core/Src/sensors.c
+++ software/STM32F103RBT6/shu-poyo/Core/Src/sensors.c
@@ -4,14 +4,16 @@
 #include "parameters.h"
 #include "global_var.h"
 
+extern ADC_HandleTypeDef hadc1;
+
 typedef struct{
     uint16_t s_offset;      // The sensor value (LED OFF)
     uint16_t s_cur;         // The current sensor value (LED ON)
     uint16_t s_prev;        // The previous sensor value (LED ON)
     uint16_t s_limit;       // The sensors limit, AKA the value when its near the wall
     uint16_t s_ref;         // The sensors reference value
-    uint16_t s_ctrl;
-    uint16_t s_noctrl;
+    uint16_t s_ctrl;        // Toggle for sensor control
+    uint16_t s_noctrl;      // Toggle for non-sensor control
 }stDIST_SEN;
 
 stDIST_SEN st_sen[DIST_SEN_NUM];
@@ -22,16 +24,22 @@ void sensors_init(void){
 
 void read_sensor_R(void){
 
+    st_sen[DIST_SEN_R].s_prev = st_sen[DIST_SEN_R].s_cur;
+
     // Read sensor value (LED OFF)
-    st_sen[DIST_SEN_R].s_offset = 0;
+    HAL_ADC_Start(&hadc1);
+    HAL_ADC_PollForConversion(&hadc1, 1000);
+    st_sen[DIST_SEN_R].s_offset = HAL_ADC_GetValue(&hadc1);
 
     // Turn LED on and wait a little
     SENSOR_OUT_R_ON;
     for (int i=0; i<1000; i++);
 
     // Read sensor value (LED ON)
-
+    HAL_ADC_Start(&hadc1);
+    HAL_ADC_PollForConversion(&hadc1, 1000);
     // Compare the two values and overwrite it into the structure
+    st_sen[DIST_SEN_R].s_cur = HAL_ADC_GetValue(&hadc1) - st_sen[DIST_SEN_R].s_offset;
 
     SENSOR_OUT_R_OFF;
 }
