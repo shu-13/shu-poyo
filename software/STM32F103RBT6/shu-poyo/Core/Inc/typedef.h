635fed7eac82ab30911b70b17525a45e869a5a95
shu-rt <shu.kirby.rt@gmail.com>
2023-02-01 14:00:08 +0900

    Adds structure and enum for robot localization

--- software/STM32F103RBT6/shu-poyo/Core/Inc/typedef.h
+++ software/STM32F103RBT6/shu-poyo/Core/Inc/typedef.h
@@ -2,6 +2,7 @@
 #ifndef __TYPEDEF_H
 #define __TYPEDEF_H
 
+/* The structure for the sensor values */
 typedef struct{
     uint16_t s_offset;      // The sensor value (LED OFF)
     uint16_t s_cur;         // The current sensor value (LED ON)
@@ -11,6 +12,23 @@ typedef struct{
     uint16_t s_ctrl;        // Toggle for sensor control
     uint16_t s_noctrl;      // Toggle for non-sensor control
 }stDIST_SEN;
+
+/* The enum for the robot's direction */
+typedef enum{
+    FRONT=0,
+    RIGHT,
+    REAR,
+    LEFT,
+    UNKNOWN,
+}enLOCAL_DIRECTION;
+
+/* The structure for the robot's position 
+// And it's facing direction */
+typedef struct{
+    short x;
+    short y;
+    enLOCAL_DIRECTION dir;
+}stPOSITION;
 
 
 #endif /* __TYPEDEF_H */
