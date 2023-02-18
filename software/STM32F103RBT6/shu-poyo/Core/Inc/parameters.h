5ac323aa26c29c0c9f0983e595c021b45686eae9
shu-rt <shu.kirby.rt@gmail.com>
2023-02-18 19:57:51 +0900

    Updates parameters

--- software/STM32F103RBT6/shu-poyo/Core/Inc/parameters.h
+++ software/STM32F103RBT6/shu-poyo/Core/Inc/parameters.h
@@ -7,26 +7,30 @@
 #define CHATTERING_WAIT (50)    // in ms
 
 // wheel params
-#define WHEEL_DIAMETER      (28.0)    // in mm
-#define WHEEL_RADIUS        (WHEEL_DIAMETER / 2.0) // in mm
+#define WHEEL_DIAMETER      (27.15)    // in mm
+#define WHEEL_RADIUS        (WHEEL_DIAMETER * 0.5) // in mm
 #define TREAD_WIDTH         (80.0)    // in mm
 
 
 // Run parameters
-#define SEARCH_SPEED (200)  // [mm/s]
+#define SEARCH_SPEED (100)  // [mm/s]
 #define SEARCH_ACCEL (1000) // [mm/s/s]
 
 // PID control things
-#define K_P     (0.1)
+#define K_P     (0.15)
 #define K_I     (0.001)
 #define K_D     (0.0)
 #define FF_GAIN (0.0)
 
 // sensor ref values
-#define SEN_REF_R   (500)
-#define SEN_REF_FR  (500)
-#define SEN_REF_FL  (500)
-#define SEN_REF_L   (500)
+// These are mostly for control
+#define SEN_TH_R   (550)       // For the front wall, when the mouse is at the edge of the section
+#define SEN_TH_FR  (250)       // For the right side, when the mouse is in the left corner
+#define SEN_TH_FL  (250)       // For the left side, when the mouse is in the right corner
+#define SEN_TH_L   (550)       // For the front wall, when the mouse is at the edge of the section
+
+#define SEN_REF_R  (200)        // For the front wall, when the mouse is at the center of the section
+#define SEN_REF_L  (200)        // For the front wall, when the mouse is at the center of the section
 
 #define __PARAMETERS_H
 
