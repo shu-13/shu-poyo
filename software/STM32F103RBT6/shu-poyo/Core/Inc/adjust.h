eefcb2ca5c7988006a2b394bc8e7c1ac0d3593dc
shu-rt <shu.kirby.rt@gmail.com>
2023-03-27 18:42:10 +0900

    Tyring to add the left hand sequence

--- software/STM32F103RBT6/shu-poyo/Core/Inc/adjust.h
+++ software/STM32F103RBT6/shu-poyo/Core/Inc/adjust.h
@@ -3,5 +3,6 @@
 #define __ADJUST_H
 
 void run_to_wall(float max_acc, float init_sp, float max_sp);
+void test_left_hand(float max_acc, float init_sp, float max_sp);
 
 #endif /* __ADJUST_H */
--- software/STM32F103RBT6/shu-poyo/Core/Src/adjust.c
+++ software/STM32F103RBT6/shu-poyo/Core/Src/adjust.c
@@ -10,11 +10,11 @@ void run_to_wall(float max_acc, float init_sp, float max_sp){
     max_speed = max_sp;
     accel = max_acc;
 
+    NEXT_DIR = NEXT_FRONT;
     // Move half of half section
-    straight(HALF_SECTION*0.25, accel, 0.0, max_speed, max_speed);
+    straight(HALF_SECTION*0.45, accel, 0.0, max_speed, max_speed);
 
     while(1){
-        get_sensor_values();
         // See if there's a front wall
         if((st_sen[DIST_SEN_L].s_cur < st_sen[DIST_SEN_L].s_ref)
             || (st_sen[DIST_SEN_R].s_cur < st_sen[DIST_SEN_R].s_ref)){
@@ -22,11 +22,32 @@ void run_to_wall(float max_acc, float init_sp, float max_sp){
             straight(HALF_SECTION, accel, max_speed, max_speed, max_speed);
         }else{
             // If there's a wall, move a little and stop
-            straight(HALF_SECTION*0.01, accel, max_speed, max_speed, 0.0);
+            straight(HALF_SECTION*0.1, accel, max_speed, max_speed, 0.0);
             RUN_MODE = MODE_STOP;
             break;
         }
-        printf("\x1B[2J"); // Clears the terminal
-        printf("\x1B[0;0H"); // Moves the cursor to the top
     }
+}
+
+void test_left_hand(float max_acc, float init_sp, float max_sp){
+    max_speed = max_sp;
+    accel = max_acc;
+
+    // Move half of half section
+    straight(HALF_SECTION*0.45, accel, 0.0, max_speed, max_speed);
+
+    while(1){
+        // See if there's a front wall
+        if((st_sen[DIST_SEN_L].s_cur < st_sen[DIST_SEN_L].s_ref)
+            || (st_sen[DIST_SEN_R].s_cur < st_sen[DIST_SEN_R].s_ref)){
+            // If there's no wall, move half section
+            straight(HALF_SECTION, accel, max_speed, max_speed, max_speed);
+        }else{
+            // If there's a wall in front, move a little and stop
+            HAL_Delay(100);
+            rotate(NEXT_RIGHT, 1);
+            HAL_Delay(100);
+        } 
+    }
+
 }
