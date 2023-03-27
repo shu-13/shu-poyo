ecca32ca799f7020b6c11cc48780cd564f55b090
shu-rt <shu.kirby.rt@gmail.com>
2023-03-27 18:40:26 +0900

    WIP: initial commit of search files

---
+++ software/STM32F103RBT6/shu-poyo/Core/Inc/search.h
@@ -0,0 +1,10 @@
+// Searching algorithms for shu-poyo
+#ifndef __SEARCH_H
+#define __SEARCH_H
+
+#include "global_var.h"
+#include "parameters.h"
+
+
+
+#endif /* __SEARCH_H */
--- software/STM32F103RBT6/shu-poyo/Debug/Core/Src/subdir.mk
+++ software/STM32F103RBT6/shu-poyo/Debug/Core/Src/subdir.mk
@@ -10,6 +10,7 @@ C_SRCS += \
 ../Core/Src/main.c \
 ../Core/Src/motors.c \
 ../Core/Src/run.c \
+../Core/Src/search.c \
 ../Core/Src/sensors.c \
 ../Core/Src/stm32f1xx_hal_msp.c \
 ../Core/Src/stm32f1xx_it.c \
@@ -23,6 +24,7 @@ OBJS += \
 ./Core/Src/main.o \
 ./Core/Src/motors.o \
 ./Core/Src/run.o \
+./Core/Src/search.o \
 ./Core/Src/sensors.o \
 ./Core/Src/stm32f1xx_hal_msp.o \
 ./Core/Src/stm32f1xx_it.o \
@@ -36,6 +38,7 @@ C_DEPS += \
 ./Core/Src/main.d \
 ./Core/Src/motors.d \
 ./Core/Src/run.d \
+./Core/Src/search.d \
 ./Core/Src/sensors.d \
 ./Core/Src/stm32f1xx_hal_msp.d \
 ./Core/Src/stm32f1xx_it.d \
@@ -55,6 +58,8 @@ Core/Src/motors.o: ../Core/Src/motors.c Core/Src/subdir.mk
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103xB -c -I../Core/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Core/Src/motors.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 Core/Src/run.o: ../Core/Src/run.c Core/Src/subdir.mk
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103xB -c -I../Core/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Core/Src/run.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Core/Src/search.o: ../Core/Src/search.c Core/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103xB -c -I../Core/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Core/Src/search.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 Core/Src/sensors.o: ../Core/Src/sensors.c Core/Src/subdir.mk
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103xB -c -I../Core/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Core/Src/sensors.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 Core/Src/stm32f1xx_hal_msp.o: ../Core/Src/stm32f1xx_hal_msp.c Core/Src/subdir.mk
